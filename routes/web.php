48aaa8ae9aa6a40f7b2427eb27930b76e3934765
patelnishit1012-sys <patelnishit1012@gmail.com>
2026-02-07 18:20:29 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return "Hello World!";
+});
 
 Route::get('/server-check', function () {
     return "You are on PORT: " . request()->getPort();
